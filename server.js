fee4c01f2ab05e546c1e145a0c4d20a43c0ad89b
Lennie <64614859+lenniecottrell@users.noreply.github.com>
2021-03-04 12:00:18 -0800

    update path to index

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 
 if (process.env.NODE_ENV === "production") {
   // Serve any static files
-  app.use("/client", express.static(path.join(__dirname, "build")));
+  app.use(express.static("client/build"));
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
