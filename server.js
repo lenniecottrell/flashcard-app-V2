9f68a24c3f21dc37feb0cba080ef817d6768cc42
Lennie <64614859+lenniecottrell@users.noreply.github.com>
2021-03-04 10:40:01 -0800

    revise server.js

--- server.js
+++ server.js
@@ -9,6 +9,11 @@ app.listen(5000, () => {
   console.log("CORS-enabled web server is listening on port 5000");
 });
 
+if (process.env.NODE_ENV === "production") {
+  // Serve any static files
+  app.use("/client", express.static(path.join(__dirname, "build")));
+}
+
 //-- ROUTES --//
 
 //create a topic DONE
@@ -340,12 +345,3 @@ app.delete("/topics/:topic/questions/:id_question", async (req, res) => {
     console.error(error.message);
   }
 });
-
-if (process.env.NODE_ENV === "production") {
-  // Serve any static files
-  app.use(express.static(path.join(__dirname, "client/build")));
-  // Handle React routing, return all requests to React app
-  app.get("*", function (req, res) {
-    res.sendFile(path.join(__dirname, "client/build", "index.html"));
-  });
-}
