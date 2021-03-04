34dc9219245b6089da552ca8bb4154dc00074bc3
Lennie <64614859+lenniecottrell@users.noreply.github.com>
2021-03-04 11:28:05 -0800

    change port variable

--- server.js
+++ server.js
@@ -5,8 +5,11 @@ const pool = require("./db");
 
 app.use(cors());
 app.use(express.json());
-app.listen(5000, () => {
-  console.log("CORS-enabled web server is listening on port 5000");
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`CORS-enabled web server is listening on port ${PORT}`);
 });
 
 // if (process.env.NODE_ENV === "production") {
