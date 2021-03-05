3e5dc373d08b41afdd1df329fcfbd6a6b39211f2
Lennie <64614859+lenniecottrell@users.noreply.github.com>
2021-03-04 18:06:56 -0800

    update db

--- db.js
+++ db.js
@@ -4,11 +4,15 @@ const user = process.env.DB_USER;
 const secret = process.env.DB_SECRET;
 
 const pool = new Pool({
-  host: process.env.DB_HOST,
+  host: process.env.DATABASE_URL,
   user: user,
   password: secret,
   port: process.env.PORT,
   database: "flashcard_app",
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 module.exports = pool;
