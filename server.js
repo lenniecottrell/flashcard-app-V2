080a7494846a4dc53e87b64b59e7aacc86654495
Lennie <64614859+lenniecottrell@users.noreply.github.com>
2021-03-05 14:59:22 -0800

    update package.json and server.js for production

--- server.js
+++ server.js
@@ -18,10 +18,14 @@ app.listen(5000, () => {
   console.log("CORS-enabled web server is listening on port 5000");
 });
 
-// if (process.env.NODE_ENV === "production") {
-//   // Serve any static files
-//   app.use("/client", express.static(path.join(__dirname, "build")));
-// }
+//production route to index in server.js
+if (process.env.NODE_ENV === "production") {
+  // Serve any static files
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 //-- ROUTES --//
 
