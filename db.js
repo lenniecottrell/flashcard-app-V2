2b063a5a12fbc3c205652ee17da6217960378cfb
Lennie <64614859+lenniecottrell@users.noreply.github.com>
2021-03-06 11:14:09 -0800

    change connection string

--- db.js
+++ db.js
@@ -1,16 +1,8 @@
 require("dotenv").config({ debug: process.env.DEBUG });
 const Pool = require("pg").Pool;
-const user = process.env.DB_USER;
-const secret = process.env.DB_SECRET;
 
 const pool = new Pool({
-  host: process.env.DATABASE_URL,
-  user: user,
-  password: secret,
-  port: process.env.DB_PORT,
-  database: "flashcard_app",
   connectionString: process.env.DATABASE_URL,
-  ssl: process.env.DATABASE_URL ? true : false,
 });
 
 module.exports = pool;
