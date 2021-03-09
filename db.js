7ec24885ddf966a5933aeca6b408842d4bce220f
Lennie <64614859+lenniecottrell@users.noreply.github.com>
2021-03-08 16:19:35 -0800

    edit database url

--- db.js
+++ db.js
@@ -3,6 +3,9 @@ const Pool = require("pg").Pool;
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 module.exports = pool;
